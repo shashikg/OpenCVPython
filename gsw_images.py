bb147a1f10458a5f6ee9dc9bb6db9a331aa64ef5
shashikg <shashikg@iitk.ac.in>
2017-09-29 15:56:29 +0530

    Getting started with images

---
+++ gsw_images.py
@@ -0,0 +1,26 @@
+import numpy as np
+import cv2
+
+#load image in grayscale
+img_gs = cv2.imread('28.jpg',0)
+
+#load image in colour
+img_cr = cv2.imread('28.jpg',1)
+
+#load image in unchanged
+img_uc = cv2.imread('28.jpg',-1)
+
+#showing the images
+cv2.imshow('grayscale',img_gs)
+cv2.imshow('colour',img_cr)
+cv2.imshow('unchanged',img_uc)
+
+#wait for t milliseconds for a key to be entered, 0 for infinite
+k = cv2.waitKey(0) & 0xFF
+
+if k == 27:         # wait for ESC key to exit
+    cv2.destroyAllWindows()
+elif k == ord('s'): # wait for 's' key to save and exit
+    #write image as a new name with 28gs.png and the image data in img_gs
+    cv2.imwrite('28gs.png',img_gs)
+    cv2.destroyAllWindows()
